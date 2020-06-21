d3cc52d3340cd7d7d854017dfd9e93d66d556b28
adeepikaa <adeepika@gmail.com>
2020-06-21 17:01:43 -0400

    updated

--- data_explore.R
+++ data_explore.R
@@ -13,17 +13,21 @@ max(bank_data$age)
 
 dist<-bank_data%>%
   group_by(age)%>%
-  summarize(n=n())%>%
+  summarize(n=n(), .groups='drop')%>%
   ggplot(aes(x=age, y=n))+
   geom_line()+
-  ggtitle(" Distribution of Age")
+  xlab("Age")+
+  ylab("Total number of people")+
+  ggtitle(" Total Distribution of Age")
 
 dist_dep<-bank_data%>%
   group_by(age)%>%
   summarize(n=n(), deposit_pct=sum(deposit=="yes")/n, .groups="drop")%>%
   ggplot(aes(x=age, y=deposit_pct))+
   geom_bar(stat="identity")+
-  ggtitle(" Distribution of Deposits across Age")
+  xlab("Age")+
+  ylab("Percent of people deposited")+
+  ggtitle(" Deposit percentage across Age")
 
 grid.arrange(dist, dist_dep, ncol=1)
 
@@ -34,14 +38,18 @@ grid.arrange(dist, dist_dep, ncol=1)
 e<-bank_data%>%
   ggplot(aes(x=education,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle(" Distribution of Deposits across Education")+
+  ggtitle(" Distribution")+
+  xlab("Education")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
 m<-bank_data%>%
   ggplot(aes(x=marital,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle(" Distribution of Deposits across Marital status")+
+  ggtitle(" Distribution")+
+  xlab("Marital Status")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
@@ -55,11 +63,16 @@ bank_data%>%
   ggplot(aes(x=job,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
   ggtitle(" Distribution of Deposits with Job")+
+  xlab("Job")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 bank_data%>%
   ggplot()+
   geom_boxplot(aes(job, age))+
+  ggtitle("Distribution of Jobs across Age")+
+  xlab("Job")+
+  ylab("Age")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
@@ -70,20 +83,23 @@ bank_data%>%
 h<-bank_data%>%
   ggplot(aes(x=housing,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle(" House loans")+
+  xlab("House Loans")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
 l<-bank_data%>%
   ggplot(aes(x=loan,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle(" Personal loans")+
+  xlab("Personal Loans")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 d<-bank_data%>%
   ggplot(aes(x=default,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle(" Credit Default")+
+  xlab("Credit Default")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 grid.arrange(h,l,d, nrow=1)
@@ -95,6 +111,8 @@ bank_data%>%
   ggplot(aes(x=contact,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
   ggtitle(" Distribution of Deposits with different types of contact")+
+  xlab("Type of Contact")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 ###########################################
@@ -104,13 +122,17 @@ bank_data%>%
 mon<-bank_data%>%
   ggplot(aes(x=month,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle("Last contact Month for current campaign")+
+  ggtitle("Last contact month")+
+  xlab("Months")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 day<-bank_data%>%
   ggplot(aes(x=day,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
-  ggtitle("Last contact day for current campaign")+
+  ggtitle("Last contact day")+
+  xlab("Days")+
+  ylab("Total number of people")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 grid.arrange(mon, day, nrow=1)
@@ -118,6 +140,8 @@ grid.arrange(mon, day, nrow=1)
 bank_data%>%
   ggplot(aes(x=campaign,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
+  xlab("Number of contacts")+
+  ylab("Total number of people")+
   ggtitle("Number of contacts for current campaign")
 
 ###########################################
@@ -126,6 +150,8 @@ bank_data%>%
 bank_data%>%
   ggplot(aes(x=poutcome,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
+  xlab("Previous Campaign Outcome")+
+  ylab("Total number of people")+
   ggtitle(" Distribution of Deposits with previous outcomes")
 
 
@@ -133,11 +159,15 @@ bank_data%>%
   ggplot(aes(x=pdays,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
   ggtitle(" Days since last contact")+
+  xlab("Days since last contact")+
+  ylab("Total number of people")+
   xlim(c(0,400))
 
 bank_data%>%
   ggplot(aes(x=previous,  group=deposit, fill=deposit))+
   geom_histogram(stat="count", bins=20, col="black", position="dodge")+
+  xlab("Number of contacts")+
+  ylab("Total number of people")+
   ggtitle(" Contacts before this campaign")
 
 
@@ -161,7 +191,7 @@ bal1<-bank_data%>%
 bal2<-bank_data%>%
   ggplot(aes(x=deposit, y=balance, col=deposit))+
   geom_boxplot()+
-  ggtitle("Distribution of Bank balance")+
+  ggtitle("Balance between 0 & 2500")+
   ylim(0, 2500)
 
 grid.arrange(bal1, bal2, nrow=1)
@@ -184,14 +214,14 @@ bank_data%>%
 b1<-bank_data%>%
   ggplot(aes(x=education,  y=balance))+
   geom_bar(stat="identity")+
-  ggtitle(" Balance for different Education")+
+  ggtitle(" Balance Vs Education")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
 b2<-bank_data%>%
   ggplot(aes(x=marital,  y=balance))+
   geom_bar(stat="identity")+
-  ggtitle(" Balance for Marital Status")+
+  ggtitle(" Balance Vs Marital Status")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 grid.arrange(b1, b2, nrow=1)
@@ -199,20 +229,17 @@ grid.arrange(b1, b2, nrow=1)
 h_b<-bank_data%>%
   ggplot(aes(x=housing,  y=balance))+
   geom_bar(stat="identity")+
-  ggtitle(" Balance with Housing loan")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
 l_b<-bank_data%>%
   ggplot(aes(x=loan,  y=balance))+
   geom_bar(stat="identity")+
-  ggtitle(" Balance with Personal loan")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 d_b<-bank_data%>%
   ggplot(aes(x=default,  y=balance))+
   geom_bar(stat="identity")+
-  ggtitle(" Balance with Credit Default")+
   theme(axis.text.x = element_text(face = "bold",
                                    size = 10, angle = 45, hjust = 1, vjust = 1))
 
--- models.R
+++ models.R
@@ -2,24 +2,33 @@
 
 # Remove the duration and pdays columns.
 # removing pdays as more than half of the customers were not contacted before
+# removing duration as it is heaily correlated to output and will be more 
+# only after deciding to make deposit
 
 bank_data_ml<-bank_data[, -c(12, 14)]
 
 # Splitting data into train, test and eval sets with 80% 20% ratio
 set.seed(123)
 
+# use 20% as evalset which will be used on final model to report performance 
 test_index <- createDataPartition(bank_data_ml$deposit, times = 1, p = 0.2, list = FALSE)
 temp <- bank_data_ml[-test_index,]
 evalset <- bank_data_ml[test_index,]
 
+
 set.seed(123)
 
+# use above obtained 80% of data to make further splits to get trainset and testset 
+# with same ratio of 80-20 split
 test_index <- createDataPartition(temp$deposit, times = 1, p = 0.2, list = FALSE)
 trainset <- temp [-test_index,]
 testset <- temp [test_index,]
 
 
 ##################################################################################
+
+# Function to create classification algorithem metrics
+# measures confusion matrix, precision, recall and F score
 
 get_result_stats<-function(x,y){
   cm<-table(Predict=x, Reference=y)
@@ -32,6 +41,7 @@ get_result_stats<-function(x,y){
 }
 
 ##################################################################################
+# Cross-validation with k-fold=5
 
 ctrl <- trainControl(method = "cv",
                      number = 5,
@@ -39,138 +49,248 @@ ctrl <- trainControl(method = "cv",
 
 ##################################################################################
 
+# KNN model 
+
+# use caret package, with cross-validation and fine tune for k
 model_knn<-  train(deposit~., data=trainset, method = "knn", 
                    trControl = ctrl, tuneGrid = data.frame(k = seq(3, 50, 2)))
+
+# best value of k
 model_knn$bestTune
 ggplot(model_knn)
 
+# predict values for testset as factors
 knn_dep<-predict(model_knn, testset)
+
+# summarize metrics
 knn_summary<-get_result_stats(knn_dep, testset$deposit)
 
+# predict values for testset as probablities 
+# used for Area under the curve calculation usinf different cutoff values
 knn_y<- predict(model_knn, testset, type="prob")[,2]
-pred <- prediction(knn_y, testset$deposit)
-roc.perf = performance(pred, measure = "tpr", x.measure = "fpr")
-knn_auc = performance(pred, measure = "auc")
+pred.knn <- prediction(knn_y, testset$deposit)
+knn_auc = performance(pred.knn, measure = "auc")
 
-plot(roc.perf, main="KNN ROC Curve")
+# plot ROC curve for different cutoff values
+roc.perf.knn = performance(pred.knn, measure = "tpr", x.measure = "fpr")
+plot(roc.perf.knn, main="KNN ROC Curve")
 abline(a=0, b= 1)
 
+# create a summary to add results after each model
 all_results<-data.frame(method="Baseline: KNN", f1score=knn_summary$f1score,
-                                           acc=knn_summary$acc, precision=knn_summary$precision,
+                        accuracy=knn_summary$acc, precision=knn_summary$precision,
                                            recall=knn_summary$recall, AUC=round(knn_auc@y.values[[1]],6))
 
 
 ##################################################################################
 
+# Naive Bayes model 
+
+# use caret package, with cross-validation 
 model_nb<-  train(deposit~., data=trainset, method = "naive_bayes", 
                    trControl = ctrl)
 
+# predict values for testset as factors
 nb_dep<-predict(model_nb, testset)
 
+# summarize metrics
 nb_summary<-get_result_stats(nb_dep, testset$deposit)
 
+# predict values for testset as probablities 
+# used for Area under the curve calculation usinf different cutoff values
 nb_y<- predict(model_nb, testset, type="prob")[,2]
 pred <- prediction(nb_y, testset$deposit)
 nb_auc = performance(pred, measure = "auc")
 
+# add results to summary
 all_results<-rbind(all_results, data.frame(method="Naive Bayes", f1score=nb_summary$f1score,
-                        acc=nb_summary$acc, precision=nb_summary$precision,
+                                           accuracy=nb_summary$acc, precision=nb_summary$precision,
                         recall=nb_summary$recall, AUC=round(nb_auc@y.values[[1]],6)))
-
+all_results%>%knitr::kable()
 
 ##################################################################################
 
+# Logistic Regression model 
 
+# use caret package, with cross-validation 
 model_glm<-  train(deposit~., data=trainset, method = "glm", 
                    trControl = ctrl)
 
+model_glm$finalModel
+
+# predict values for testset as factors
 glm_dep<-predict(model_glm, testset)
+
+# summarize metrics
 glm_summary<-get_result_stats(glm_dep, testset$deposit)
 
+# predict values for testset as probablities 
+# used for Area under the curve calculation usinf different cutoff values
 glm_y<- predict(model_glm, testset, type="prob")[,2]
 pred <- prediction(glm_y, testset$deposit)
 glm_auc = performance(pred, measure = "auc")
 
+# add results to summary
 all_results<-rbind(all_results, data.frame(method="Logistic Regression", f1score=glm_summary$f1score,
-                                           acc=glm_summary$acc, precision=glm_summary$precision,
+                                           accuracy=glm_summary$acc, precision=glm_summary$precision,
                                            recall=glm_summary$recall, AUC=round(glm_auc@y.values[[1]],6)))
 
-
+all_results%>%knitr::kable()
 ##################################################################################
 
+# CART: Tree model 
 
-
-model_tree<-  train(deposit~., data=trainset, method="rpart",
+# use caret package, with cross-validation and fine tune for cp(complex parameter)
+model_tree<-  train(deposit~., data=trainset, method="rpart", tuneGrid= data.frame(cp=seq(0, 0.5, 0.05)),
                          trControl = ctrl)
+# best value of cp
 model_tree$bestTune
 ggplot(model_tree)
 
+# plot(model_tree$finalModel)
+# text(model_tree$finalModel)
+
+# predict values for testset as factors
 tree_dep<-predict(model_tree, testset)
+
+# summarize metrics
 tree_summary<-get_result_stats(tree_dep, testset$deposit)
 
+# predict values for testset as probablities 
+# used for Area under the curve calculation usinf different cutoff values
 tree_y<- predict(model_tree, testset, type="prob")[,2]
 pred <- prediction(tree_y, testset$deposit)
 tree_auc = performance(pred, measure = "auc")
 
+# add results to summary
 all_results<-rbind(all_results, data.frame(method="CART", f1score=tree_summary$f1score,
-                                           acc=tree_summary$acc, precision=tree_summary$precision,
+                                           accuracy=tree_summary$acc, precision=tree_summary$precision,
                                            recall=tree_summary$recall, AUC=round(tree_auc@y.values[[1]],6)))
-
+all_results%>%knitr::kable()
 
 ##################################################################################
 
+# Random Forest model 
 
+# the randomForest package is used with cross-validation k-fold=5
 model_rf<-  randomForest(deposit~., data=trainset,  mtry=4,
                    trControl = ctrl)
 
+# predict values for testset as factors
 rf_dep<-predict(model_rf, testset)
+
+# summarize metrics
 rf_summary<-get_result_stats(rf_dep, testset$deposit)
 
+# predict values for testset as probablities 
+# used for Area under the curve calculation usinf different cutoff values
 rf_y<- predict(model_rf, testset, type="prob")[,2]
 pred <- prediction(rf_y, testset$deposit)
-roc.perf = performance(pred, measure = "tpr", x.measure = "fpr")
 rf_auc = performance(pred, measure = "auc")
 
+# add results to summary
 all_results<-rbind(all_results, data.frame(method="Random Forest", f1score=rf_summary$f1score,
-                                           acc=rf_summary$acc, precision=rf_summary$precision,
+                                           accuracy=rf_summary$acc, precision=rf_summary$precision,
                                            recall=rf_summary$recall, AUC=round(rf_auc@y.values[[1]],6)))
 all_results%>%knitr::kable()
 
+# The variable importance plot shows which variables 
+# have had an impact in the classification
 varImpPlot(model_rf)
 
-plot(roc.perf, main="RF ROC Curve")
-abline(a=0, b= 1)
+
 ##################################################################################
 
+# Gradient Boosting model 
+
+# use caret package, with cross-validation and fine tune for interaction.depth & n.trees
 gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9), 
                         n.trees = (1:30)*50, 
                         shrinkage = 0.1,
                         n.minobsinnode = 20)
 model_gbm<-  train(deposit~., data=trainset, method="gbm",
                    trControl = ctrl, tuneGrid = gbmGrid)
+
+# best value of hyper-parameters
 model_gbm$bestTune
-ggplot(model_gbm)
 
+# predict values for testset as factors
 gbm_dep<-predict(model_gbm, testset)
-gbm_summary<-get_result_stats(gbm_dep, testset$deposit)
-gbm_y<- predict(model_gbm, testset, type="prob")[,2]
-pred <- prediction(gbm_y, testset$deposit)
-gbm_auc = performance(pred, measure = "auc")
 
+# summarize metrics
+gbm_summary<-get_result_stats(gbm_dep, testset$deposit)
+
+# predict values for testset as probabilities 
+# used for Area under the curve calculation using different cutoff values
+gbm_y<- predict(model_gbm, testset, type="prob")[,2]
+pred.gbm <- prediction(gbm_y, testset$deposit)
+gbm_auc = performance(pred.gbm, measure = "auc")
+
+# add results to summary
 all_results<-rbind(all_results, data.frame(method="Gradient Boost", f1score=gbm_summary$f1score,
-                                           acc=gbm_summary$acc, precision=gbm_summary$precision,
+                                           accuracy=gbm_summary$acc, precision=gbm_summary$precision,
                                            recall=gbm_summary$recall, AUC=round(gbm_auc@y.values[[1]],6)))
 
 all_results%>%knitr::kable()
 
-##################################################################################
+
+# plot ROC curve for different cutoff values
+roc.perf.gbm = performance(pred.gbm, measure = "tpr", x.measure = "fpr")
+plot(roc.perf.gbm, main="Gradient Boosting ROC Curve")
+abline(a=0, b= 1)
+
+# plot of KNN ROC curve to compare performance
+plot(roc.perf.gbm, main="GBM & KNN ROC Curves")
+par(new=TRUE)
+plot(roc.perf.knn)
+abline(a=0, b= 1)
 
 
-gbm_summ<-get_result_stats(gbm_eval, evalset$deposit)
-gbm_evalset<- predict(model_gbm, evalset, type="prob")[,2]
-pred_evalset <- prediction(gbm_evalset, evalset$deposit)
-gbm_auc_eval = performance(pred_evalset, measure = "auc")
+#####################################################################
+# Cut off analysis: Analyze different cut-off levels to define
+# the two class labels for best F score
+
+# user defined function to sweep cutoff values for optimum Fscore
+get_cutoff<-function(x){
+  
+  cutoff<-seq(0.1, 0.9, 0.025)
+  f1s<-sapply(cutoff, function(z){
+    y<-ifelse(x>z,1,0)
+    scores<-get_result_stats(y, testset$deposit)
+    scores$f1score
+  })
+  return(cutoff[which.max(f1s)])
+}
+
+# Perform cut-off analysis on the GBM output
+final_cutoff<-get_cutoff(gbm_y) 
+
+# predict outcomes based on fine tuned cutoff
+final_y<-ifelse(gbm_y>final_cutoff,1,0)
+
+# final summary for train/test data
+final_stats<-get_result_stats(final_y, testset$deposit)
 
 
-data.frame(Accuracy=gbm_summ$acc, Precision=gbm_summ$precision, 
-           Recall=gbm_summ$recall, F1Score=gbm_summ$f1score, AUC=round(gbm_auc_eval@y.values[[1]],6))
+# add results to summary
+all_results<-rbind(all_results, data.frame(method="Gradient Boost(Cut-off analysis)", f1score=final_stats$f1score,
+                                           accuracy=final_stats$acc, precision=final_stats$precision,
+                                           recall=final_stats$recall, AUC=round(gbm_auc@y.values[[1]],6)))
+
+all_results%>%knitr::kable()
+
+#####################################################################
+# Final Model on the evaluation dataset
+
+#Predict probability outputs for the Evaluation dataset 
+eval_y<- predict(model_gbm, evalset, type="prob")[,2]
+
+# Use cutoff obtained from GBM to define class labels
+evalset_deposit<-ifelse(eval_y>final_cutoff,1,0)
+
+# Get final summary for evaluation set
+final_summary<-get_result_stats(evalset_deposit, evalset$deposit)
+
+# Final summary of the evaluation of the model put in easy to read format
+data.frame(F1Score=final_summary$f1score, Accuracy=final_summary$acc, Precision=final_summary$precision, 
+           Recall=final_summary$recall)
